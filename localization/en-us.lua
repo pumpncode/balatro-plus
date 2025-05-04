943cc89104edf4b593ce3ee19026e4843dbadeb3
SomeCoder99 <r1zone.el4nsy4h@gmail.com>
2025-05-04 15:10:16 +0700

    added Dash and Rainbow Voucher

--- localization/en-us.lua
+++ localization/en-us.lua
@@ -522,6 +522,21 @@ return {
           "{C:attention}+#2#{} card slot",
         },
       },
+      v_bplus_dash = {
+        name = "Dash",
+        text = {
+          "When {C:attention}Blind{} is",
+          "skipped, earn {C:money}$#1#{}",
+        },
+      },
+      v_bplus_rainbow = {
+        name = "Rainbow",
+        text = {
+          "When {C:attention}Blind{} is skipped,",
+          "give any {C:dark_edition}edition",
+          "to random Joker",
+        },
+      },
     },
 
     Blind = {
@@ -639,7 +654,7 @@ return {
       tag_bplus_bounty = {
         name = "Bounty Tag",
         text = {
-          "Earn {X:money,C:white}X#1#{} {C:money}dollars{} of the {C:attention}Blinds",
+          "Earn {X:money,C:white} X#1# {} {C:money}dollars{} of the {C:attention}Blinds",
           "reward when defeated",
         },
       },
