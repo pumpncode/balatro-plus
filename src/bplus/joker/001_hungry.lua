2fb647b3a9f6ab91439afd3354d502e704623286
SomeCoder99 <r1zone.el4nsy4h@gmail.com>
2025-05-07 07:44:46 +0700

    adjusted Hungry Joker

--- src/bplus/joker/001_hungry.lua
+++ src/bplus/joker/001_hungry.lua
@@ -1,5 +1,5 @@
 return {
-  config = { extra = 1, Xmult = 1 },
+  config = { extra = 0.5, Xmult = 1 },
   rarity = 2,
   cost = 7,
 
@@ -8,52 +8,51 @@ return {
 
   loc_vars = function(_, infoq, card)
     infoq[#infoq + 1] = { set = "Other", key = "bplus_food_jokers" }
-    return { vars = { card.ability.x_mult } }
+    return { vars = { card.ability.extra, card.ability.x_mult } }
   end,
 
   calculate = function(_, card, ctx)
     if ctx.ending_shop and not ctx.blueprint then
-      local destroyed = {}
+      local destroyed
       for _, joker in ipairs(G.jokers.cards) do
         if joker.config.center.bplus_food_joker then
           joker.getting_sliced = true
-          table.insert(destroyed, joker)
+          destroyed = joker
+          break
         end
       end
 
-      if next(destroyed) then
-        BPlus.u.joker_destroyed_trigger(destroyed)
-        for _, joker in ipairs(destroyed) do
-          G.E_MANAGER:add_event(Event {
-            func = function()
-              play_sound("tarot1")
-              joker.T.r = -0.2
-              joker:juice_up(0.3, 0.4)
-              joker.states.drag.is = true
-              joker.children.center.pinch.x = true
-              card:juice_up(0.4, 0.5)
+      if destroyed then
+        BPlus.u.joker_destroyed_trigger { destroyed }
+        G.E_MANAGER:add_event(Event {
+          func = function()
+            play_sound("tarot1")
+            destroyed.T.r = -0.2
+            destroyed:juice_up(0.3, 0.4)
+            destroyed.children.center.pinch.x = true
+            destroyed.states.drag.is = true
+            card:juice_up(0.4, 0.5)
 
-              G.E_MANAGER:add_event(Event {
-                trigger = "after",
-                delay = 0.3,
-                blockable = false,
-                func = function()
-                  G.jokers:remove_card(joker)
-                  joker:remove()
-                  joker = nil
-                  return true
-                end,
-              })
-              return true
-            end,
-          })
+            G.E_MANAGER:add_event(Event {
+              trigger = "after",
+              delay = 0.3,
+              blockable = false,
+              func = function()
+                G.jokers:remove_card(destroyed)
+                destroyed:remove()
+                destroyed = nil
+                return true
+              end,
+            })
+            return true
+          end,
+        })
 
-          card_eval_status_text(joker, "jokers", nil, nil, nil, {
-            message = localize("k_eaten_ex"),
-          })
+        card_eval_status_text(destroyed, "jokers", nil, nil, nil, {
+          message = localize("k_eaten_ex"),
+        })
 
-          card.ability.x_mult = card.ability.x_mult + card.ability.extra
-        end
+        card.ability.x_mult = card.ability.x_mult + card.ability.extra
 
         card_eval_status_text(card, "extra", nil, nil, nil, {
           message = localize { type = "variable", key = "a_xmult", vars = { card.ability.x_mult } },
